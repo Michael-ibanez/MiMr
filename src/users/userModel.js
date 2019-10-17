88ca229a3209ff9f414a37476974089ff9c3a9e6
Michael Ibanez <michael_ibanez@baylor.edu>
2019-10-17 15:33:46 -0500

    Added new npm modules for login and registration. If you're seeing this commit i'm still okay. I'm at lik 10% health

--- src/users/userModel.js
+++ src/users/userModel.js
@@ -1,14 +1,20 @@
-// grab the things we need
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// create a schema
-const user = mongoose.Schema({
-    name: { type: String, required: true },
-    password: { type: String, required: true }
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
-
-// the schema is useless so far
-// we need to create a model using it
-// make this available to our users in our Node applications
-mongoose.model('User', user);
-module.exports = mongoose.model('User');
+let User = mongoose.model("users", UserSchema);
+module.exports = User;
