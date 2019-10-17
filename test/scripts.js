43de960c01f7fa9565303d456c6293c04b9343f8
Manuel Reyes <manuel.reyes.harmony2013@gmail.com>
2019-10-17 01:27:00 -0500

    Add files via upload

--- test/scripts.js
+++ test/scripts.js
@@ -3,9 +3,22 @@
 alert("Just type a value into the input box, and it will update the total field");
 
 var inputVal = 0 ;
+var todayHours = 0;
+var totalHours = 0;
 
 function getInputValue(){
     inputVal = document.getElementById("hours").value;
-    var newHours = document.createTextNode(inputVal);
     document.getElementById("hoursWorked").innerHTML = inputVal;
+    totalHours += parseInt(inputVal);
+    document.getElementById("hoursTotalWeek").innerHTML = totalHours;
+    return true;
 }
+
+
+function newDate(){
+    var newT = document.createElement("main-table");
+    var newText = document.createTextNode("Water");
+    newT.appendChild(newText);
+    document.getElementById("column1").appendChild(newT);
+}
+
